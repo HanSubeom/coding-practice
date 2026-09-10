da9266b24ed3efe463d0a698492c56b4006fec97
HanSubeom <rrenaissances@naver.com>
2026-09-10 21:55:55 +0900

    Add delete_memo feature with enumerate and pop

--- memo_app.py
+++ memo_app.py
@@ -12,16 +12,38 @@ def show_memos():
     for line in lines:
         print(line.strip())
 
-choice = input("1. Add memo  2. Show memos  3. Exit\nChoose: ")
+def delete_memo():
+    file = open("memo.txt", "r")
+    lines = file.readlines()
+    file.close()
+
+    print("--- Your Memos ---")
+    for i, line in enumerate(lines):
+        print(str(i + 1) + ": " + line.strip())
+
+    choice = int(input("Enter the number to delete: "))
+
+    lines.pop(choice - 1)
+
+    file = open("memo.txt", "w")
+    for line in lines:
+        file.write(line)
+    file.close()
+
+    print("Memo deleted!")
+
+choice = input("1. Add memo  2. Show memos  3. Exit  4. Delete memo\nChoose: ")
 
 while choice != "3":
     if choice == "1":
         add_memo()
     elif choice == "2":
         show_memos()
+    elif choice == "4":
+        delete_memo()
     else:
         print("Invalid choice.")
-    
-    choice = input("1. Add memo  2. Show memos  3. Exit\nChoose: ")
+
+    choice = input("1. Add memo  2. Show memos  3. Exit  4. Delete memo\nChoose: ")
 
 print("Goodbye!")
